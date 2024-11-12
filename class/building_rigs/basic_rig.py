6edcc33ba74d1d851eab9e695769da63dd51992f
Daniel Ruiz Vargas <danielkenobi3d@gmail.com>
2024-11-11 21:08:47 -0500

    basic rig

--- class/building_rigs/basic_rig.py
+++ class/building_rigs/basic_rig.py
@@ -1,7 +1,9 @@
 from RMPY.rig import rigSingleJoint
+from RMPY.rig import rigFK
 import pymel.core as pm
 
 
 selection = pm.ls(selection=True)
-rig_joint = rigSingleJoint.RigSingleJoint()
+# rig_joint = rigSingleJoint.RigSingleJoint()
+rig_joint = rigFK.RigFK()
 rig_joint.create_point_base(*selection)
