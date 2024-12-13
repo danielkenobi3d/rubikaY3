2a7dd6ce112af5edb539e1d1e2e58768f832e4fc
itsmallynn <lynndemers@hotmail.com>
2024-12-13 11:33:24 -0500

    Homework exercise 1

--- class/lynn/ex 1.py
+++ class/lynn/ex 1.py
@@ -1,1 +1,17 @@
-hrgjhgf
+import pymel.core as pm
+
+def create_cube():
+    locator = pm.ls('locator*', type='transform')
+
+    if not locator:
+        print("Please place locator.")
+        return
+
+    for locator in locator:
+        cube = pm.polyCube(name=f"{locator}_cube")[0]
+
+        pm.matchTransform(cube, locator)
+
+        pm.delete(locator)
+
+create_cube()
