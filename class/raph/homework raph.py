9c1cf78344d262f75656c0e6b3db7e452a94e3db
raphy-shark <raph.pietresson@gmail.com>
2024-12-13 16:30:44 -0500

    homework raph

--- class/raph/homework raph.py
+++ class/raph/homework raph.py
@@ -32,3 +32,18 @@ random_cube()
 
 
 
+import pymel.core as pm
+
+def cube_staircase (n):
+    for i in range(n):
+        for j in range(i + 1):
+            x = i * 2
+            y = j * 2
+            z = 0
+
+            mirrored_x = -x
+
+            cube = pm.polyCube(w=1, h=1, d=1)[0]
+            pm.move(mirrored_x, y, z)
+
+cube_staircase (8)
