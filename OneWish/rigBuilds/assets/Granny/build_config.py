5b2553a1f40ffe5bf6ee0d9223a0a6cb61c7e310
ArthurYangy <yangyifei000320@gmail.com>
2025-02-11 12:34:27 -0500

    grannychange

--- OneWish/rigBuilds/assets/Granny/build_config.py
+++ OneWish/rigBuilds/assets/Granny/build_config.py
@@ -13,6 +13,7 @@ build = {
         ('load shapes', ['rig_build.load_shapes_data'])
     ],
     'finalize': [
+        ('build visibility switches', ['visibility_switches.build']),
         ('cleanup', ['rig_build.cleanup']),
         ('custom finalize', ['rig_build.custom_finalize'])
     ],
