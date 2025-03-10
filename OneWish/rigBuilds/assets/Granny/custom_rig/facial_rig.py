edc9281232201fc0bc72171e3b5cc295b4aa6aa9
ArthurYangy <yangyifei000320@gmail.com>
2025-03-10 18:16:08 -0400

    1

---
+++ OneWish/rigBuilds/assets/Granny/custom_rig/facial_rig.py
@@ -0,0 +1,14 @@
+import pymel.core as pm
+#from RMPY.rig.biped.rig import rigEyesAim
+#from RMPY.rig.biped.rig import rigEyesSpaceSwitch
+from RMPY.rig import rigStaticLayer
+from RMPY.rig.facial import rigJaw
+from RMPY.core import data_save_load
+
+def build():
+    jaw_static_layers = rigStaticLayer.StaticLayer('body', name='jaw')
+    rig_jaw = rigJaw.RigJaw(rig_system=jaw_static_layers.rig_system)
+    rig_jaw.create_point_base('C_jawstatic00_reference_pnt', 'C_jawstatic01_reference_pnt')
+
+    data_save_load.load_skin_cluster(*jaw_static_layers.static_geometries)
+
--- OneWish/rigBuilds/assets/Granny/rig_build.py
+++ OneWish/rigBuilds/assets/Granny/rig_build.py
@@ -1,9 +1,14 @@
 from rigBuilds.assets.male.custom_rig import rigBiped
+from OneWish.rigBuilds.assets.default_character.rig_build import facial_rig
 import pymel.core as pm
-
+import pymel.core as importlib
+importlib.reload(facial_rig)
 
 def build_biped_rig():
     rig_biped = rigBiped.RigByped()
     rig_biped.build()
     pm.parent('geo', 'rig')
     pm.parent('environment', 'rig')
+
+def custom_rig():
+    facial_rig.build()
